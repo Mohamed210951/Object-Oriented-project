acb080f311a962086c08de6d51130a7739bafb61
Mohamed210951 <100765820+Mohamed210951@users.noreply.github.com>
2022-03-28 21:17:55 +0200

    old back end

--- System/Back End.php
+++ System/Back End.php
@@ -11,22 +11,30 @@ function GetLastId(string $fileName) {
 	}
 	return $max;
 }
-function IdIsThere(string $FileName, int $Value){
-	$File = fopen("../Files/".$FileName, 'r');
-	while($Line = fgets($File))	{
-		$Array = explode('~',$Line);
-		if($Array[0] == $Value) return 1;
-	}
-	return 0;
-}
-function UserNameIsThere(string $FileName,string $Value){
-	$File = fopen("../Files/".$FileName, 'r');
-	while($Line = fgets($File))	{
-		$Array = explode('~',$Line);
-		if($Array[2] == $Value) 
-		{
-			return $Array;
+
+function DisplayTable(array $List) {
+	echo "<table>";
+	for ($i=0; $i < count($List); $i++) { 
+		echo "<tr>";
+		for ($j=0; $j < count($List[$i]); $j++) { 
+			echo "<th>".$List[$i][$j]."</th>";
 		}
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+/**
+ * 
+ * @param string $FileName the Name of the file
+ * @param string $Value the value you want to search for
+ * @param int $Index the index { in the file format } of the value you want to search for
+ * @return mixed Return the line if value founded else return NULL
+ */
+function ValueIsThere(string $FileName,string $Value,int $Index){
+	$File = fopen("../Files/".$FileName, 'r');
+	while($Line = fgets($File))	{
+		$Array = explode('~',$Line);
+		if($Array[$Index] == $Value) return $Line;
 	}
 	return null;
 }
@@ -56,26 +64,6 @@ function FileDelete(string $FileName, string $Data){
     $contents = str_replace($Data, "", $contents);
     file_put_contents("../Files/".$FileName, $contents);
 }
-
-function Login(string $FileName,string $UserName,string $Password) {
-	if($Array = UserNameIsThere($FileName,$UserName))
-    {
-        if($Array[3] == $Password)
-        {
-            $Type = $Array[1];
-            FileWrite("UserNow.txt",$Type);
-            header("Location:MainMenu.php");
-        }
-        else
-        {
-            echo "Wrong UserName or Password!!";
-        }
-    }
-    else
-    {
-        echo "Wrong UserName or Password!!";
-    }
-}
 function FromTypeGetServis(string $Type) {
 	$List = GetAllContent("User Type.txt");
 	$IdType = "-1";
@@ -96,10 +84,6 @@ function FromTypeGetServis(string $Type) {
 	}
 	return $Servis;
 }
-function SignUp(string $UserName,string $Password,string $Type){
-	$newUser = new User(GetLastId("User.txt") + 1,$Type,$UserName,$Password);
-	$newUser->Add();
-}
 function Encrypt($Word, $Key) {
     $Result = "";
     for ($i = 0; $i < strlen($Word); $i++) {
@@ -115,312 +99,4 @@ function Decrypt($Word, $Key) {
         $Result .= $c;
     }
     return $Result;
-	/***
-	 * 
-	 */
-}
-class Person {
-    protected ?int $Id;
-    protected ?string $Name;
-	function getId(): int {
-		return $this->Id;
-	}
-	function setId(?int $Id): int {
-		if($Id <= 0) return 0;
-		$this->Id = $Id;
-		return 1;
-	}
-	function getName(): string {
-		return $this->Name;
-	}
-	function setName(?string $Name): int {
-		if(str_contains($Name,'~')) return 0;
-		$this->Name = $Name;
-		return 1;
-	}
-}
-interface File {
-	public function Add($input1 = null,$input2 = null,$input3 = null,$input4 = null);
-	public function Update($input1 = null,$input2 = null,$input3 = null,$input4 = null);
-	public function Searsh($input1 = null,$input2 = null,$input3 = null,$input4 = null);
-	public function Delete($input1 = null,$input2 = null,$input3 = null,$input4 = null);
-}
-class User extends Person implements File {
-	private $Password;
-	private $Type;
-	public function __construct(int $Id = null,string $Type = null, string $Name = null, string $Password = null) {
-		if($Id != null) {
-			$this->setId($Id);
-			$this->setName($Name);
-			$this->setPassword($Password);
-			$this->setType($Type);
-		}
-	}
-	public function AllIsSet(): int {
-		if(is_null($this->Id)) return 0;
-		if(is_null($this->Name)) return 0;
-		if(is_null($this->Password)) return 0;
-		if(is_null($this->Type)) return 0;
-		return 1;
-	}
-	public function ToString(): string {
-		$Line = $this->Id . '~' . $this->Type . '~' . $this->Name . '~' . $this->Password . "~\r\n";
-		return $Line;
-	}
-	public static function StringToUser(string $Line){
-		$Array = explode('~',$Line);
-		$user = new User(intval($Array[0]),$Array[1],$Array[2],$Array[3]);
-		return $user;
-	}
-	function getPassword() {
-		return $this->Password;
-	}
-	function setPassword($Password) {
-		if(str_contains($Password,'~')) return 0;
-		$this->Password = $Password;
-		return 1;
-	}
-	function getType() {
-		return $this->Type;
-	}
-	function setType($Type) {
-		$List = GetAllContent("User Type.txt");
-		$flag = 0;
-		for ($i=0; $i < count($List); $i++) { 
-			$Array = explode('~',$List[$i]);
-			if($Array[1] == $Type) $flag = 1;
-		}
-		if($flag == 0) return 0;
-		$this->Type = $Type;
-		return 1;
-	}
-	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		if($this->AllIsSet()) {
-			if(!UserNameIsThere("User.txt",$this->Name)) {
-				FileAdd("User.txt",$this->ToString());
-				FileWrite("UserNow.txt",$this->Type);
-				header("Location:MainMenu.php");
-			}
-			else {
-				echo "This UserName is alrady exists!!";
-			}
-		}
-		else {
-			echo "Please Try again but prevent using '~'!!";
-		}
-	}
-	
-	/**
-	 * 
-	 * @param string $input1 Note1
-	 * @param float $input2 Note2
-	 */
-	function Update($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		// Code
-	}
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		// Code
-	}
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		// Code
-	}
-}
-class Product extends Person implements File {
-	private ?float $Cost;
-	function getCost(): float {
-		return $this->Cost;
-	}
-	function setCost(?float $Cost): int {
-		if($Cost < 0) return 0;
-		$this->Cost = $Cost;
-		return 1;
-	}
-	function __construct(int $Id = null,float $Cost = null,string $Name = null) {
-		if($Id!=null)
-		{
-			$this->setId($Id);
-			$this->setName($Name);
-			$this->setCost($Cost);
-		}
-	}
-	public function ToString() {
-		$String = $this->Id . "~" . $this->Cost  . "~" . $this->Name . "~\r\n";
-		return $String;	
-	}
-	public function AllIsSet() {
-		if($this->Id == null) return 0;
-		if($this->Name == null) return 0;
-		if($this->Cost == null) return 0;
-		return 1;
-	}
-	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		if($this->Name == null) return 0;
-		if($this->Cost == null) return 0;
-		$Last_Id_In_file = GetLastId("Product.txt");
-    	$this->setId($Last_Id_In_file+1);
-    	$isexist= UserNameIsThere("Product.txt",$this->Name);
-    	if($isexist==null) FileAdd("Product.txt",$this->ToString());
-    	else {
-			echo "the product is already exist";
-			return 0;
-		}
-		return 1;
-	}
-	function Update($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-}
-class Child extends Person implements File {
-	
-	private ?string $Password;
-	private ?int $Age;
-	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		// Code
-	}
-	function Update($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function getPassword(): string {
-		return $this->Password;
-	}
-	function setPassword(?string $Password): int {
-		if(str_contains($Password,'~')) return 0;
-		$this->Password = $Password;
-		return 1;
-	}
-	function getAge(): int {
-		return $this->Age;
-	}
-	function setAge(?int $Age): int {
-		if(str_contains($Age,'~')) return 0;
-		$this->Age = $Age;
-		return 1;
-	}
-	function __construct(int $Id = null, string $Name = null, string $Age = null,string $Password = null) {
-		if(!$this->setId($Id)) $this->Id = null;
-		if(!$this->setName($Name)) $this->Name = null;
-		if(!$this->setPassword($Password))$this->Name = null;
-		if(!$this->setAge($Age))$this->Name = null;
-	}
-	public function AllIsSet()
-	{
-		if(is_null($this->Id)) return 0;
-		if(is_null($this->Name)) return 0;
-		if(is_null($this->Age)) return 0;
-		if(is_null($this->Password)) return 0;
-		return 1;
-	}
-	public function ToString() {
-		$String = $this->Id."~".$this->Name."~".$this->Age."~".$this->Password."~\r\n";
-		return $String;
-	}
-}
-class order extends Person implements File {
-	private float $total;
-	private int $ClientId;
-	private string $date;
-	public function AllIsSet() {
-		if($this->Id == null) return 0;
-		if($this->Name == null) return 0;
-		if($this->total == null) return 0;
-		if($this->ClientId == null) return 0;
-		if($this->Date == null) return 0;
-		return 1;
-	}
-	/**
-	 *
-	 * @param mixed $input1
-	 * @param mixed $input2
-	 * @param mixed $input3
-	 * @param mixed $input4
-	 *
-	 * @return mixed
-	 */
-	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		if($this->Name==null) return 0;
-		if($this->Id==null) return 0;
-		if($this->total==null) return 0;
-		if($this->ClientId==null) return 0;
-		if($this->date==null) return 0;
-		
-	}
-	
-	function Update($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-	}
-	
-	/**
-	 *
-	 * @param mixed $input1
-	 * @param mixed $input2
-	 * @param mixed $input3
-	 * @param mixed $input4
-	 *
-	 * @return mixed
-	 */
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-	}
-	
-	/**
-	 *
-	 * @param mixed $input1
-	 * @param mixed $input2
-	 * @param mixed $input3
-	 * @param mixed $input4
-	 *
-	 * @return mixed
-	 */
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-	}
-	/**
-	 * 
-	 * @return string
-	 */
-	function getDate(): string {
-		return $this->date;
-	}
-	
-	function setDate(string $date): int  {
-		if($date <= 0) return 0;
-		$this->date = $date;
-		return 1;
-	}
-
-	function gettotal(): float {
-		return $this->total;
-	}
-
-
-	function settotal(float $total): int {
-		if($total <= 0) return 0;
-		$this->total = $total;
-		return 1;
-	}
-
-	function getClientId(): int {
-		return $this->ClientId;
-	}
-	
-
-	function setClientId(int $ClientId): int  {
-		if($ClientId <= 0) return 0;
-		$this->ClientId = $ClientId;
-		return 1;
-	}
-
-	public static function StringToObject(string $String) {
-
-	}
-
 }
