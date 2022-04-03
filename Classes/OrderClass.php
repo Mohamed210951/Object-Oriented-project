b7ac815f4c739d39174934f08df2fa74f3871c00
GeorgeElkess <100765793+GeorgeElkess@users.noreply.github.com>
2022-04-03 22:53:26 +0200

    ..

--- Classes/OrderClass.php
+++ Classes/OrderClass.php
@@ -1,118 +1,6 @@
 <?php
 include_once "../System/Back End.php";
 include_once "PersonClass.php";
-<<<<<<< Updated upstream
-include_once "OrderDetails.php";
-class Product extends Person implements File {
-	private ?float $Cost;
-	function getCost(): float {
-		return $this->Cost;
-	}
-	function setCost(?float $Cost): int {
-		if($Cost < 0) return 0;
-		$this->Cost = $Cost;
-		return 1;
-	}
-	function __construct(int $Id = null,float $Cost = null,string $Name = null) {
-		if($Id!=null)
-		{
-			$this->setId($Id);
-			$this->setName($Name);
-			$this->setCost($Cost);
-		}
-	}
-	public function ToString() {
-		$String = $this->Id . "~" . $this->Cost  . "~" . $this->Name . "~\r\n";
-		return $String;	
-	}
-	public function AllIsSet() {
-		if($this->Id == null) return 0;
-		if($this->Name == null) return 0;
-		if($this->Cost == null) return 0;
-		return 1;
-	}
-	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		if($this->Name == null) return 0;
-		if($this->Cost == null) return 0;
-		$Last_Id_In_file = GetLastId("Product.txt");
-    	$this->setId($Last_Id_In_file+1);
-    	$isexist= ValueIsThere("Product.txt",$this->Name,2);
-    	if($isexist==null) FileAdd("Product.txt",$this->ToString());
-    	else {
-			echo "the product is already exist";
-			return 0;
-		}
-		return 1;
-	}
-	function Update($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-}
-class Child extends Person implements File {
-	
-	private ?string $Password;
-	private ?int $Age;
-	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		// Code
-	}
-	function Update($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-		//Code
-	}
-	function getPassword(): string {
-		return $this->Password;
-	}
-	function setPassword(?string $Password): int {
-		if(str_contains($Password,'~')) return 0;
-		$this->Password = $Password;
-		return 1;
-	}
-	function getAge(): int {
-		return $this->Age;
-	}
-	function setAge(?int $Age): int {
-		if(str_contains($Age,'~')) return 0;
-		$this->Age = $Age;
-		return 1;
-	}
-	function __construct(int $Id = null, string $Name = null, string $Age = null,string $Password = null) {
-		if(!$this->setId($Id)) $this->Id = null;
-		if(!$this->setName($Name)) $this->Name = null;
-		if(!$this->setPassword($Password))$this->Name = null;
-		if(!$this->setAge($Age))$this->Name = null;
-	}
-	public function AllIsSet()
-	{
-		if(is_null($this->Id)) return 0;
-		if(is_null($this->Name)) return 0;
-		if(is_null($this->Age)) return 0;
-		if(is_null($this->Password)) return 0;
-		return 1;
-	}
-	public function ToString() {
-		$String = $this->Id."~".$this->Name."~".$this->Age."~".$this->Password."~\r\n";
-		return $String;
-	}
-}
-class order extends Person implements File {
-	private float $total;
-	private int $ClientId;
-	private string $date;
-	public function AllIsSet() {
-		if($this->Id==null) return 0;
-		if($this->ClientId==null) return 0;
-=======
 include_once "OrderDetailsClass.php";
 class order extends Person implements File {
 	private ?float $total = 0;
@@ -122,7 +10,6 @@ class order extends Person implements File {
 		if($this->Id==null) return 0;
 		if($this->ClientId==0) return 0;
 		if($this->date=="") return 0;
->>>>>>> Stashed changes
 		return 1;
 	}
 	/**
@@ -135,11 +22,7 @@ class order extends Person implements File {
 	 * @return mixed
 	 */
     public function ToString() {
-<<<<<<< Updated upstream
-		$String = $this->Id."~".$this->ClientId."~\r\n";
-=======
 		$String = $this->Id."~".$this->ClientId."~".$this->date."~\r\n";
->>>>>>> Stashed changes
 		return $String;
 	}
 	function Add($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
@@ -166,11 +49,7 @@ class order extends Person implements File {
     static function FromStringToObject($string)
 	{
        $Array_Of_String=explode("~",$string);
-<<<<<<< Updated upstream
-	   $Order=new order(intval($Array_Of_String[0]),intval($Array_Of_String[1]));
-=======
 	   $Order=new order(intval($Array_Of_String[0]),intval($Array_Of_String[1]),($Array_Of_String[2]));
->>>>>>> Stashed changes
 	   return $Order;
 	}
 	
@@ -182,42 +61,6 @@ class order extends Person implements File {
 		{
 			$this->Id=$Order->getId();
 		}
-<<<<<<< Updated upstream
-		if($this->getClientId()=="")
-		{
-		   $this->ClientId=$Order->getClientId();
-		}
-		FileUpdate("Order.txt",$Order->ToString(),$this->ToString());
-	}
-	
-	/**
-	 *
-	 * @param mixed $input1
-	 * @param mixed $input2
-	 * @param mixed $input3
-	 * @param mixed $input4
-	 *
-	 * @return mixed
-	 */
-	function Searsh($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-	}
-	
-	/**
-	 *
-	 * @param mixed $input1
-	 * @param mixed $input2
-	 * @param mixed $input3
-	 * @param mixed $input4
-	 *
-	 * @return mixed
-	 */
-	function Delete($input1 = null, $input2 = null, $input3 = null, $input4 = null) {
-	}
-	/**
-	 * 
-	 * @return string
-	 */
-=======
 		if($this->getClientId()==0)
 		{
 		   $this->ClientId=$Order->getClientId();
@@ -240,7 +83,6 @@ class order extends Person implements File {
 	 }
 	}
 	
->>>>>>> Stashed changes
 	function getDate(): string {
 		return $this->date;
 	}
@@ -262,26 +104,4 @@ class order extends Person implements File {
 		return 1;
 	}
 
-<<<<<<< Updated upstream
-    function setProductId(int $ProductId): int  {
-		if($ProductId <= 0) return 0;
-		$this->ProductId = $ProductId;
-		return 1;
-	}
-    function getProductId(): int {
-		return $this->ProductId;
-	}
-
-	public static function StringToObject(string $String) {
-
-	}
-
-
-	/**
-	 * 
-	 * @return string
-	 */
-
-=======
->>>>>>> Stashed changes
 }
